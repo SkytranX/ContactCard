f3cd7378454c656586b4671fe9b4c87f6c91815c
Steven Tran <StevenSkytran@gmail.com>
2019-09-15 20:46:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/contactcard/ContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.contactcard;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContactActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_screen);
+    }
+}
